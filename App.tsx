d5319bafb4410085c039aae61ae91ea4d4440d0a
abdenassar <abdenassaramimi99@gmail.com>
2023-11-27 00:53:54 +0100

    fix splash screen

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
-
+import SplashScreen from 'react-native-splash-screen';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 import Home from './src/app/screens/home';
 
@@ -10,6 +10,8 @@ function App(): JSX.Element {
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
+
+  useEffect(() => SplashScreen.hide(), []);
 
   return (
     <SafeAreaView style={{flex: 1}}>
