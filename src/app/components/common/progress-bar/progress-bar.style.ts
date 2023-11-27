4d9465a7e2bf96bfeb6e12f8988acbaa8c65bccf
abdenassar <abdenassaramimi99@gmail.com>
2023-11-27 15:30:08 +0100

    fixing progress bar to match

--- src/app/components/common/progress-bar/progress-bar.style.ts
+++ src/app/components/common/progress-bar/progress-bar.style.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components/native';
 import {ThemeType} from '../../../../utils/theme';
 import {MainText} from '../text';
+import {Dimensions} from 'react-native';
 
 export const ProgressBarWrapper = styled.View<{theme: ThemeType}>`
   width: 100%;
@@ -17,19 +18,18 @@ export const ProgressItemWrapper = styled.View<{
   background-color: ${({theme, active}) =>
     active ? theme.tertiary : theme.background};
   border-radius: 50px;
-  height: 30px;
-  width: 30px;
+  height: 24px;
+  width: 24px;
   justify-content: center;
   align-items: center;
-  border-width: 1px;
   border-radius: 50px;
   background-color: ${({theme, active}) =>
     active ? theme.primary : theme.stepsIndicator};
 `;
 
 export const ProgressItemInnerCircle = styled(MainText)<{theme: ThemeType}>`
-  height: 20px;
-  width: 20px;
+  height: 8px;
+  width: 8px;
   border-radius: 50px;
   background-color: ${({theme}) => theme.background};
 `;
@@ -38,8 +38,11 @@ export const LineSeparator = styled.View<{
   theme: ThemeType;
   active?: boolean;
 }>`
-  height: 3px;
-  width: 50px;
+  z-index: -1;
+  margin-left: -5px;
+  margin-right: -5px;
+  height: 6px;
+  width: ${Dimensions.get('screen').width / 3 + 10}px;
   background-color: ${({theme, active}) =>
     active ? theme.primary : theme.stepsIndicator};
 `;
