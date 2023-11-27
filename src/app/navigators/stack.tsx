1e49084e2ec8e6cdbd46d787602fe982be31ca82
abdenassar <abdenassaramimi99@gmail.com>
2023-11-27 11:51:17 +0100

    adding required stacks to render conditionaly

---
+++ src/app/navigators/stack.tsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/react-in-jsx-scope */
+import {createStackNavigator} from '@react-navigation/stack';
+import Home from '../screens/home';
+
+const Stack = createStackNavigator();
+
+export const OnboardingStack = () => (
+  <Stack.Navigator screenOptions={{headerShown: false}}>
+    <Stack.Screen name="OnBoarding" component={Home} />
+  </Stack.Navigator>
+);
+
+export const UnAuthenticationStacks = () => {
+  return (
+    <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Screen name="Login" component={Home} />
+      <Stack.Screen name="Signup" component={Home} />
+      <Stack.Screen name="ForgotPassword" component={Home} />
+    </Stack.Navigator>
+  );
+};
+
+export const AuthenticatedStack = () => {
+  return (
+    <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Screen name="HomeTab" component={Home} />
+      <Stack.Screen name="Chat" component={Home} />
+      <Stack.Screen name="Payment" component={Home} />
+    </Stack.Navigator>
+  );
+};
