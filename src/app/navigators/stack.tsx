c6c6f5d4d1fceceff7a87a34f5f3ee64cf1df95e
abdenassar <abdenassaramimi99@gmail.com>
2023-11-28 01:19:09 +0100

    adding auth screens to stack

--- src/app/navigators/stack.tsx
+++ src/app/navigators/stack.tsx
@@ -2,21 +2,24 @@
 import {createStackNavigator} from '@react-navigation/stack';
 import Home from '../screens/home';
 import Onboarding from '../screens/onboarding/onboarding';
+import Login from '../screens/auth/login/login';
+import SignUp from '../screens/auth/signup/signup';
+import ResetPassword from '../screens/auth/reset/reset-password';
 
 const Stack = createStackNavigator();
 
 export const OnboardingStack = () => (
   <Stack.Navigator screenOptions={{headerShown: false}}>
-    <Stack.Screen name="OnBoarding" component={Onboarding} />
+    <Stack.Screen name="OnBoardingg" component={Onboarding} />
   </Stack.Navigator>
 );
 
 export const UnAuthenticationStacks = () => {
   return (
     <Stack.Navigator screenOptions={{headerShown: false}}>
-      <Stack.Screen name="Login" component={Home} />
-      <Stack.Screen name="Signup" component={Home} />
-      <Stack.Screen name="ForgotPassword" component={Home} />
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Signup" component={SignUp} />
+      <Stack.Screen name="ForgotPassword" component={ResetPassword} />
     </Stack.Navigator>
   );
 };
