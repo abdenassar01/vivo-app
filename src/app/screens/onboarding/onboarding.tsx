c3c833c6a5c16db90d0184c79d31773f18162a9c
abdenassar <abdenassaramimi99@gmail.com>
2023-11-28 17:06:06 +0100

    adding onboarding screen design

--- src/app/screens/onboarding/onboarding.tsx
+++ src/app/screens/onboarding/onboarding.tsx
@@ -1,11 +1,29 @@
-import {View, Text} from 'react-native';
 import React from 'react';
+import {t} from 'i18next';
+import {
+  OnBoardingContent,
+  OnBoardingHeading,
+  OnBoardingText,
+  OnBoardingWrapper,
+} from './onboarding.style';
+import Button from '../../components/common/form-fields/button/button';
 
 const Onboarding = () => {
   return (
-    <View>
-      <Text>onboarding</Text>
-    </View>
+    <OnBoardingWrapper
+      source={require('../../../assets/images/onboarding.png')}
+      // eslint-disable-next-line react-native/no-inline-styles
+      imageStyle={{width: '100%', height: 470}}>
+      <OnBoardingContent>
+        <OnBoardingHeading>{t('welcome-header')}</OnBoardingHeading>
+        <OnBoardingText>{t('Welcome-text')}</OnBoardingText>
+        <Button
+          onPress={() => console.log('Save onBoarding state')}
+          text={t('button-next-text')}
+          width="100%"
+        />
+      </OnBoardingContent>
+    </OnBoardingWrapper>
   );
 };
 
