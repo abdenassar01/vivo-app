58bc116cf132f5db99243bac1475811d2032372f
abdenassar <abdenassaramimi99@gmail.com>
2023-11-29 18:00:21 +0100

    addings home [quick access] tabs

--- src/app/screens/home/home.style.ts
+++ src/app/screens/home/home.style.ts
@@ -1,4 +1,36 @@
 import styled from 'styled-components/native';
 import {ThemeType} from '../../../utils/theme';
+import FastImage from 'react-native-fast-image';
+import {Heading} from '../../components/common/text';
+import {Dimensions} from 'react-native';
 
 export const HomeWrapper = styled.ScrollView<{theme: ThemeType}>``;
+
+export const TabsWrapper = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  gap: 10px;
+  flex-wrap: wrap;
+  margin-top: 20px;
+`;
+
+export const Tab = styled.TouchableOpacity<{color: string}>`
+  width: ${(Dimensions.get('screen').width - 60) / 2}px;
+  height: 82px;
+  border-radius: 7px;
+  justify-content: center;
+  align-items: center;
+  gap: 7px;
+  background-color: ${({color}) => color};
+`;
+
+export const TabIcon = styled(FastImage)`
+  width: 40px;
+  height: 40px;
+`;
+
+export const TabLabel = styled(Heading)<{theme: ThemeType}>`
+  font-size: 9px;
+  text-align: center;
+  color: ${({theme}) => theme.background};
+`;
--- src/app/screens/home/home.tsx
+++ src/app/screens/home/home.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import Header from '../../components/core/header/header';
 import {MainText} from '../../components/common/text';
 import {AppWrapper} from '../../../utils/shared-styles';
-import {HomeWrapper} from './home.style';
+import {HomeWrapper, Tab, TabIcon, TabLabel, TabsWrapper} from './home.style';
 import PointsProgressChart from '../../components/core/chart/points-progress-chart';
+import {t} from 'i18next';
 
 const Home = () => {
   return (
@@ -11,6 +12,31 @@ const Home = () => {
       <Header openDrower />
       <HomeWrapper>
         <PointsProgressChart progress={75} />
+        <TabsWrapper>
+          <Tab color="#80BD0A">
+            <TabIcon
+              resizeMode="stretch"
+              source={require('../../../assets/icons/quiz-tab.png')}
+            />
+            <TabLabel>{t('home-quiz-link')}</TabLabel>
+          </Tab>
+          <Tab color="#20A9E3">
+            <TabIcon
+              source={require('../../../assets/icons/exchange-tab.png')}
+            />
+            <TabLabel>{t('home-conversion-link')}</TabLabel>
+          </Tab>
+          <Tab color="#6E6F72">
+            <TabIcon source={require('../../../assets/icons/orders-tab.png')} />
+            <TabLabel>{t('home-demads-link')}</TabLabel>
+          </Tab>
+          <Tab color="#FFB400">
+            <TabIcon
+              source={require('../../../assets/icons/ratings-tab.png')}
+            />
+            <TabLabel>{t('home-evaluation-link')}</TabLabel>
+          </Tab>
+        </TabsWrapper>
         <MainText>home</MainText>
       </HomeWrapper>
     </AppWrapper>
