2b0d1248f04e843a12b03ed0944ab93fc053f36e
abdenassar <abdenassaramimi99@gmail.com>
2023-11-30 00:41:26 +0100

    loader tiny fix

--- src/app/components/common/loader/screen-loader.tsx
+++ src/app/components/common/loader/screen-loader.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import {useTheme} from 'styled-components';
 import {Swing} from 'react-native-animated-spinkit';
 import {LoaderWrapper} from './loader.style';
+import {StatusBar} from 'react-native';
 
 const ScreenLoader = () => {
   const theme = useTheme();
 
   return (
     <LoaderWrapper>
+      <StatusBar backgroundColor={theme.primary} />
       <Swing size={48} color={theme.background} />
     </LoaderWrapper>
   );
