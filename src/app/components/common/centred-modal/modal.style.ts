c2e4dde50f6d7c13f63ab0cbf2161262a5546358
abdenassar <abdenassaramimi99@gmail.com>
2023-12-10 01:39:07 +0100

    fix modal close icon

--- src/app/components/common/centred-modal/modal.style.ts
+++ src/app/components/common/centred-modal/modal.style.ts
@@ -1,7 +1,6 @@
 import styled from 'styled-components/native';
 import {ThemeType} from '../../../../utils/theme';
 import FastImage from 'react-native-fast-image';
-import i18next from 'i18next';
 
 export const ModalWrapper = styled.View<{theme: ThemeType}>`
   width: 100%;
@@ -16,10 +15,10 @@ export const ChildrenWrapper = styled.View`
   margin-top: 40px;
 `;
 
-export const CloseModal = styled.TouchableOpacity`
+export const CloseModal = styled.TouchableOpacity<{lang: string}>`
   position: absolute;
-  right: ${i18next.language === 'ar' ? 'auto' : '24px'};
-  left: ${i18next.language === 'ar' ? '24px' : 'auto'};
+  right: ${({lang}) => (lang === 'ar' ? 'auto' : '24px')};
+  left: ${({lang}) => (lang === 'ar' ? '24px' : 'auto')};
   top: 24px;
 `;
 
