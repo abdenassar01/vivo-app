ae30c0634a798accd09c3e1d577846fb347553d5
abdenassar01 <abdenassaramimi99@gmail.com>
2024-01-01 14:36:39 +0100

    fix progress bar

--- src/app/components/common/progress-bar/progress-bar.style.ts
+++ src/app/components/common/progress-bar/progress-bar.style.ts
@@ -1,19 +1,22 @@
-import styled from 'styled-components/native';
-import {ThemeType} from '../../../../utils/theme';
-import {MainText, SecondaryHeading} from '../text';
-import {Dimensions} from 'react-native';
+import styled from "styled-components/native";
+import { ThemeType } from "../../../../utils/theme";
+import { MainText, SecondaryHeading } from "../text";
+import { Dimensions } from "react-native";
 
-export const ProgressBarWrapper = styled.View<{theme: ThemeType; lang: string}>`
+export const ProgressBarWrapper = styled.View<{
+  theme: ThemeType;
+  lang: string;
+}>`
   width: 100%;
   justify-content: center;
   align-items: center;
-  flex-direction: ${({lang}) => (lang === 'ar' ? 'row-reverse' : 'row')};
+  flex-direction: ${({ lang }) => (lang === "ar" ? "row-reverse" : "row")};
 `;
 
 export const ProgressBarHeaderText = styled(SecondaryHeading)<{
   theme: ThemeType;
 }>`
-  color: ${({theme}) => theme.primary};
+  color: ${({ theme }) => theme.primary};
   font-size: 15px;
 `;
 
@@ -21,7 +24,7 @@ export const ProgressItemWrapper = styled.View<{
   theme: ThemeType;
   active: boolean;
 }>`
-  background-color: ${({theme, active}) =>
+  background-color: ${({ theme, active }) =>
     active ? theme.tertiary : theme.background};
   border-radius: 50px;
   height: 24px;
@@ -29,26 +32,27 @@ export const ProgressItemWrapper = styled.View<{
   justify-content: center;
   align-items: center;
   border-radius: 50px;
-  background-color: ${({theme, active}) =>
+  background-color: ${({ theme, active }) =>
     active ? theme.primary : theme.stepsIndicator};
 `;
 
-export const ProgressItemInnerCircle = styled(MainText)<{theme: ThemeType}>`
+export const ProgressItemInnerCircle = styled(MainText)<{ theme: ThemeType }>`
   height: 8px;
   width: 8px;
   border-radius: 50px;
-  background-color: ${({theme}) => theme.background};
+  background-color: ${({ theme }) => theme.background};
 `;
 
 export const LineSeparator = styled.View<{
   theme: ThemeType;
   active?: boolean;
+  nbrSteps: number;
 }>`
   z-index: -1;
   margin-left: -5px;
   margin-right: -5px;
   height: 6px;
-  width: ${Dimensions.get('screen').width / 3 + 10}px;
-  background-color: ${({theme, active}) =>
+  width: ${({ nbrSteps }) => Dimensions.get("screen").width / nbrSteps - 10}px;
+  background-color: ${({ theme, active }) =>
     active ? theme.primary : theme.stepsIndicator};
 `;
