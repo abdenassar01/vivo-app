a1d76ffcd6a12a80e9ccb53e357f88d962e72011
abdenassar01 <abdenassaramimi99@gmail.com>
2024-01-02 15:11:45 +0100

    fixing questions options components

--- src/app/components/core/question-options/question-options.tsx
+++ src/app/components/core/question-options/question-options.tsx
@@ -1,62 +1,43 @@
-import React from 'react';
+import React from "react";
 import {
   OptionSelectedIcon,
   OptionText,
   QuestionOptionItem,
   QuestionsOptionsWrapper,
-} from './question-options.style';
-import {Control, useController} from 'react-hook-form';
-import {Option} from '../../../../../types/option';
-import i18next from 'i18next';
+} from "./question-options.style";
+import { Control, useController } from "react-hook-form";
 
 interface Props {
   name: string;
   control: Control<any>;
-  options: Option[];
+  options: string[];
 }
 
-const QuestionOptions = ({options, control, name}: Props) => {
+const QuestionOptions = ({ options, control, name }: Props) => {
   const {
-    field: {onChange, value},
+    field: { onChange, value },
   } = useController({
     name,
     control,
-    defaultValue: [],
+    defaultValue: options[0],
   });
-
-  const handleOptionPress = (option: Option) => {
-    const isExist =
-      value.filter((item: Option) => item.id === option.id).length > 0;
-    if (isExist) {
-      onChange(value.filter((item: Option) => item.id !== option.id));
-    } else {
-      onChange([...value, option]);
-    }
-  };
 
   return (
     <QuestionsOptionsWrapper>
       {React.Children.toArray(
-        options.map(option => (
+        options.map((option) => (
           <QuestionOptionItem
-            onPress={() => handleOptionPress(option)}
-            selected={
-              value.filter((item: Option) => item.id === option.id).length > 0
-            }>
-            <OptionText
-              selected={
-                value.filter((item: Option) => item.id === option.id).length > 0
-              }>
-              {i18next.language === 'fr' ? option.text : option.textAr}{' '}
-            </OptionText>
-            {value.filter((item: Option) => item.id === option.id).length >
-              0 && (
+            onPress={() => onChange(option)}
+            selected={value === option}
+          >
+            <OptionText selected={value === option}>{option}</OptionText>
+            {value === option && (
               <OptionSelectedIcon
-                source={require('../../../../assets/icons/correct.png')}
+                source={require("../../../../assets/icons/correct.png")}
               />
             )}
           </QuestionOptionItem>
-        )),
+        ))
       )}
     </QuestionsOptionsWrapper>
   );
