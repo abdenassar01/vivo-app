e5215e037a884efff33f41e693cb901b8aa4aa9f
abdenassar01 <abdenassaramimi99@gmail.com>
2024-01-05 15:08:34 +0100

    update fetching quizes

--- src/app/screens/quiz/quiz.tsx
+++ src/app/screens/quiz/quiz.tsx
@@ -19,38 +19,24 @@ import { t } from "i18next";
 import Button from "../../components/common/form-fields/button/button";
 import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
-import { getQuizzes } from "../../services/Quiz";
+import { QuizType, getQuizzes } from "../../services/Quiz";
 import { UserAuth } from "../../contexts/AuthContext";
 import { Swing } from "react-native-animated-spinkit";
 import { useTheme } from "styled-components";
 import { useLangStore } from "../../../stores/lang";
+import { useQuery } from "react-query";
 
 const Quiz = () => {
   const { navigate } = useNavigation<StackNavigationProp<any>>();
   const { user } = UserAuth();
-  const [data, setData] = useState<any[]>();
-  const [isLoading, setIsLoading] = useState<boolean>(true);
   const isMounted = useRef(true);
   const { primary } = useTheme();
   const { currentLang } = useLangStore();
 
-  useEffect(() => {
-    isMounted.current = true;
-
-    const fetchData = async () => {
-      const res = await getQuizzes(user?.id || "");
-      if (isMounted.current) {
-        setIsLoading(false);
-        setData(res);
-        console.log(res);
-      }
-    };
-
-    fetchData();
-    return () => {
-      isMounted.current = false;
-    };
-  }, [user]);
+  const { data, isLoading } = useQuery<QuizType[]>(
+    ["getting user quizes", user?.id],
+    async () => getQuizzes(user?.id || "")
+  );
 
   return (
     <AppWrapper>
